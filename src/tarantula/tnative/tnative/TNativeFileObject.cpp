ffba25deb1f462131817798ef4391203f32540bc
Tony Mason <fsgeek@cs.ubc.ca>
2019-07-28 16:33:30 -0700

    native file object now builds clean

--- src/tarantula/tnative/tnative/TNativeFileObject.cpp
+++ src/tarantula/tnative/tnative/TNativeFileObject.cpp
@@ -3,61 +3,69 @@
 #pragma warning(push)
 #pragma warning(disable:26436 26447) // SAL annotations do not raise 
 
-TNativeGenerator::TNativeGenerator() noexcept
+
+const TNativeFileObject::MemoryTag TNativeFileObject::DefaultMemoryTag = { 'o','f','N','T' };
+
+TNativeFileObject::TNativeFileObject() noexcept
 {
 	this->m_FileObject = nullptr;
 }
 
-TNativeGenerator::~TNativeGenerator() noexcept
+TNativeFileObject::~TNativeFileObject() noexcept
 {
-	if (nullptr != this->m_FileObject) {
-		ObDereferenceObject(this->m_FileObject);
-		this->m_FileObject = nullptr;
+}
+
+#pragma warning(push)
+#pragma warning(disable: )
+#pragma warning(disable: 6014) // it's not a leak
+#pragma warning(disable: 26400 26409) // it doesn't like me using new
+_Use_decl_annotations_
+TNativeFileObject* TNativeFileObject::CreateTNativeFileObject(PFILE_OBJECT FileObject) noexcept
+{
+	TNativeFileObject *tnfo = new TNativeFileObject;
+	while (nullptr != tnfo) {
+		tnfo->m_FileObject = FileObject;
 	}
-}
-
-
-TNativeDirectoryObject::TNativeDirectoryObject() noexcept
-{
-}
-
-TNativeDirectoryObject::~TNativeDirectoryObject() noexcept
-{
-}
-
-TNativeDirectoryObject* TNativeDirectoryObject::CreateTNativeDirectoryObject(_In_ PFILE_OBJECT FileObject) noexcept
-{
-	UNREFERENCED_PARAMETER(FileObject);
 	return nullptr;
 }
 
+void TNativeFileObject::DeleteTNativeFileObject() noexcept
+{
+	this->m_FileObject = nullptr;
+	delete this;
+}
+
 _Use_decl_annotations_
-NTSTATUS TNativeDirectoryObject::Read(void* Buffer, size_t Offset, ULONG Length, ULONG Flags, ULONG Key) noexcept 
+NTSTATUS TNativeFileObject::Read(void* Buffer, size_t Offset, ULONG Length, ULONG Flags, ULONG Key) noexcept
 {
 	UNREFERENCED_PARAMETER(Buffer);
 	UNREFERENCED_PARAMETER(Offset);
 	UNREFERENCED_PARAMETER(Length);
 	UNREFERENCED_PARAMETER(Flags);
 	UNREFERENCED_PARAMETER(Key);
+
 	return STATUS_INVALID_DEVICE_REQUEST;
 }
 
-NTSTATUS TNativeDirectoryObject::Write(_In_reads_bytes_(Length) void* Buffer, _In_ size_t Offset, _In_ ULONG Length, _In_ ULONG Flags, ULONG Key) noexcept
+_Use_decl_annotations_
+NTSTATUS TNativeFileObject::Write(void* Buffer, size_t Offset, ULONG Length, ULONG Flags, ULONG Key) noexcept
 {
 	UNREFERENCED_PARAMETER(Buffer);
 	UNREFERENCED_PARAMETER(Offset);
 	UNREFERENCED_PARAMETER(Length);
 	UNREFERENCED_PARAMETER(Flags);
 	UNREFERENCED_PARAMETER(Key);
-	return STATUS_INVALID_DEVICE_REQUEST;
 
+	return STATUS_INVALID_DEVICE_REQUEST;
 }
 
-NTSTATUS TNativeDirectoryObject::QueryDirectory(_Out_writes_bytes_(Length) void* Buffer, _In_ ULONG Length, _In_ ULONG FileIndex) noexcept
+_Use_decl_annotations_
+NTSTATUS TNativeFileObject::QueryDirectory(void* Buffer, ULONG Length, ULONG FileIndex) noexcept
 {
 	UNREFERENCED_PARAMETER(Buffer);
 	UNREFERENCED_PARAMETER(Length);
 	UNREFERENCED_PARAMETER(FileIndex);
+
 	return STATUS_INVALID_DEVICE_REQUEST;
 }
 
--- src/tarantula/tnative/tnative/TNativeFileObject.h
+++ src/tarantula/tnative/tnative/TNativeFileObject.h
@@ -1,77 +1,26 @@
 #pragma once
 #include <ntifs.h>
 
-class TNativeGenerator
+class TNativeFileObject
 {
-	//
-	// A "generator" is anything we manage that can generate data
-	//
-	// For Windows, there are really two APIs for generating data:
-	//  (1) the usual read/write interface
-	//  (2) the directory enumeration interface
-	//
-	//
-
-	PFILE_OBJECT *m_FileObject; // we always need a file object in order to do anything
-
-public:
-	TNativeGenerator() noexcept;
-	virtual ~TNativeGenerator() noexcept;
-
-#pragma warning(push)
-#pragma warning(disable:26436 26447) // SAL annotations do not raise 
-	virtual NTSTATUS Read(_Out_writes_bytes_(Length) void *Buffer, _In_ size_t Offset, _In_ ULONG Length, _In_ ULONG Flags = 0, _In_ ULONG Key = 0) noexcept {
-		//
-		// Default is "not implemented"
-		//
-		/*
-		struct {
-			ULONG Length;
-            ULONG POINTER_ALIGNMENT Key;
-#if defined(_WIN64)
-            ULONG Flags;
-#endif
-            LARGE_INTEGER ByteOffset;
-        } Read;
-		*/
-		UNREFERENCED_PARAMETER(Buffer);
-		UNREFERENCED_PARAMETER(Offset);
-		UNREFERENCED_PARAMETER(Length);
-		UNREFERENCED_PARAMETER(Flags);
-		UNREFERENCED_PARAMETER(Key);
-		return STATUS_INVALID_DEVICE_REQUEST;
+	union MemoryTag {
+		UCHAR tag[4];
+		ULONG tagvalue;
 	};
 
-	virtual NTSTATUS Write(_In_reads_bytes_(Length) void *Buffer, _In_ size_t Offset, _In_ ULONG Length, _In_ ULONG Flags = 0, ULONG Key = 0) noexcept {
-		UNREFERENCED_PARAMETER(Buffer);
-		UNREFERENCED_PARAMETER(Offset);
-		UNREFERENCED_PARAMETER(Length);
-		UNREFERENCED_PARAMETER(Flags);
-		UNREFERENCED_PARAMETER(Key);
-		return STATUS_INVALID_DEVICE_REQUEST;
-	}
+	static const MemoryTag DefaultMemoryTag;
 
-	virtual NTSTATUS QueryDirectory(_Out_writes_bytes_(Length) void *Buffer, _In_ ULONG Length, _In_ ULONG FileIndex = 0) noexcept {
-		/*
-		struct {
-			ULONG Length;
-			PUNICODE_STRING FileName;
-			FILE_INFORMATION_CLASS FileInformationClass;
-			ULONG POINTER_ALIGNMENT FileIndex;
-		} QueryDirectory;
-		*/
-		UNREFERENCED_PARAMETER(Buffer);
-		UNREFERENCED_PARAMETER(Length);
-		UNREFERENCED_PARAMETER(FileIndex);
-		return STATUS_INVALID_DEVICE_REQUEST;
-	}
-
-	// TODO: there are other functions common to all generators: EAs, ACLs, Information.
+#pragma warning(push)
+#pragma warning(disable:26447) // prefast annotations do not throw exceptions; neither does this function
+	void* operator new(_In_ size_t size, _In_ void* memory) noexcept {
+		UNREFERENCED_PARAMETER(size); return memory;
+	};
 #pragma warning(pop)
-};	
 
-class TNativeFileObject : public TNativeGenerator
-{
+	__drv_allocatesMem(Mem)	void* operator new(_In_ size_t size) noexcept { return ExAllocatePoolWithTag(PagedPool, size, DefaultMemoryTag.tagvalue); };
+	void operator delete(__drv_freesMem(mem) void* mem) { ExFreePoolWithTag(mem, DefaultMemoryTag.tagvalue); }
+
+
 	struct FileContextNonPaged {
 		FSRTL_ADVANCED_FCB_HEADER header;
 		SECTION_OBJECT_POINTERS sop;
@@ -88,27 +37,21 @@ class TNativeFileObject : public TNativeGenerator
 	TNativeFileObject() noexcept;
 	~TNativeFileObject() noexcept;
 
+	PFILE_OBJECT m_FileObject;
 public:
 	static TNativeFileObject* CreateTNativeFileObject(_In_ PFILE_OBJECT FileObject) noexcept;
+	void DeleteTNativeFileObject() noexcept;
 
-	NTSTATUS Read(_Out_writes_bytes_(Length) void* Buffer, _In_ size_t Offset, _In_ ULONG Length, _In_ ULONG Flags = 0, _In_ ULONG Key = 0) noexcept override;
-	NTSTATUS Write(_In_reads_bytes_(Length) void* Buffer, _In_ size_t Offset, _In_ ULONG Length, _In_ ULONG Flags = 0, ULONG Key = 0) noexcept override;
-	NTSTATUS QueryDirectory(_Out_writes_bytes_(Length) void* Buffer, _In_ ULONG Length, _In_ ULONG FileIndex = 0) noexcept override;
-};
+	NTSTATUS Read(_Out_writes_bytes_(Length) void* Buffer, _In_ size_t Offset, _In_ ULONG Length, _In_ ULONG Flags = 0, _In_ ULONG Key = 0) noexcept;
+	NTSTATUS Write(_In_reads_bytes_(Length) void* Buffer, _In_ size_t Offset, _In_ ULONG Length, _In_ ULONG Flags = 0, ULONG Key = 0) noexcept;
+	NTSTATUS QueryDirectory(_Out_writes_bytes_(Length) void* Buffer, _In_ ULONG Length, _In_ ULONG FileIndex = 0) noexcept;
 
-class TNativeDirectoryObject : public TNativeGenerator
-{
-	FSRTL_ADVANCED_FCB_HEADER m_Header;
+	// we don't implement these, so we delete the default implementations.
+	TNativeFileObject(const TNativeFileObject&) = delete;
+	TNativeFileObject& operator=(const TNativeFileObject& Registry) = delete;
+	TNativeFileObject(TNativeFileObject&&) = delete;
+	TNativeFileObject& operator=(TNativeFileObject&&) = delete;
 
-
-	TNativeDirectoryObject() noexcept;
-	~TNativeDirectoryObject() noexcept;
-
-public:
-	static TNativeDirectoryObject* CreateTNativeDirectoryObject(_In_ PFILE_OBJECT FileObject) noexcept;
-
-	NTSTATUS Read(_Out_writes_bytes_(Length) void* Buffer, _In_ size_t Offset, _In_ ULONG Length, _In_ ULONG Flags = 0, _In_ ULONG Key = 0) noexcept override;
-	NTSTATUS Write(_In_reads_bytes_(Length) void* Buffer, _In_ size_t Offset, _In_ ULONG Length, _In_ ULONG Flags = 0, ULONG Key = 0) noexcept override;
-	NTSTATUS QueryDirectory(_Out_writes_bytes_(Length) void* Buffer, _In_ ULONG Length, _In_ ULONG FileIndex = 0) noexcept override;
 
 };
+
