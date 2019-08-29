4d803c2bc0dbc3f376ae991778f6737661260689
Tony Mason <fsgeek@gatech.edu>
2019-08-28 20:41:53 -0700

    Add test harness

---
+++ src/hobo/test/hobo/testhobo.c
@@ -0,0 +1,67 @@
+//
+// Hobo File System
+//
+// (C) Copyright 2019 Tony Mason
+//
+// Licensed under the Aranae project license
+// https://github.com/fsgeek/Araneae/blob/master/LICENSE
+//
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <uuid/uuid.h>
+#include <pthread.h>
+#include "munit.h"
+#include <errno.h>
+
+#if !defined(__notused)
+#define __notused __attribute__((unused))
+#endif //
+
+
+static MunitResult
+test_one(
+    const MunitParameter params[] __notused,
+    void *prv __notused)
+{
+    return MUNIT_OK;
+}
+
+#define TEST(_name, _func, _params)             \
+    {                                           \
+        .name = (_name),                        \
+        .test = (_func),                        \
+        .setup = NULL,                          \
+        .tear_down = NULL,                      \
+        .options = MUNIT_TEST_OPTION_NONE,      \
+        .parameters = (_params),                     \
+    }
+
+int
+main(
+    int argc,
+    char **argv)
+{
+    static MunitTest tests[] = {
+        TEST((char *)(uintptr_t)"/one", test_one, NULL),
+        TEST(NULL, NULL, NULL),
+    };
+    static const MunitSuite suite = {
+        .prefix = (char *)(uintptr_t)"/hobo",
+        .tests = tests,
+        .suites = NULL,
+        .iterations = 1,
+        .options = MUNIT_SUITE_OPTION_NONE,
+    };
+
+    return munit_suite_main(&suite, NULL, argc, argv);
+}
+
